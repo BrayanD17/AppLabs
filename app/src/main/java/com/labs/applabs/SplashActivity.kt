6990245a58f825b87d18ff3c51600fb3252a326e
Brayan Gutierrez Dinarte <38222878+BrayanD17@users.noreply.github.com>
2025-03-28 23:02:04 -0600

    Inicio de proyecto
    Se agregaron estilos de botones, splash screen, login view y demas ajustes para hacer la vista de botones

---
+++ app/src/main/java/com/labs/applabs/SplashActivity.kt
@@ -0,0 +1,30 @@
+package com.labs.applabs
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class SplashActivity : AppCompatActivity() {
+    private val SPLASH_TIME_OUT: Long = 2000 // 2 segundos
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        // Forzar que el contenido no invada la barra de estado o navegación
+        ViewCompat.setOnApplyWindowInsetsListener(window.decorView) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, SPLASH_TIME_OUT)
+    }
+}
