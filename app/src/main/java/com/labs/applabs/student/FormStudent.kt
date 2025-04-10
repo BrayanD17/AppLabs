d573d726c57bea9f0c2e434d14b48a4924c53e87
Samantha Acuña <samantha23@estudiantec.cr>
2025-04-10 07:51:13 -0600

    actualizacion

--- app/src/main/java/com/labs/applabs/student/FormStudent.kt
+++ app/src/main/java/com/labs/applabs/student/FormStudent.kt
@@ -1,28 +1,21 @@
 package com.labs.applabs.student
 
-import android.content.Intent
 import android.os.Bundle
-import android.view.View
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.labs.applabs.R
 
-class FormActivity : AppCompatActivity() {
+class FormStudent : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_form)
+        setContentView(R.layout.activity_form_student)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-    }
-
-    fun Siguiente(view: View){
-        val intent : Intent = Intent(this@FormActivity, com.labs.applabs.student.FormStudent::class.java)
-        startActivity(intent);
     }
 }
