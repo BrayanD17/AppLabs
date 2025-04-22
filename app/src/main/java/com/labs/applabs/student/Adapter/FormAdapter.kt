cac5e3cd90b9d3ea61660e35ed4e18ca8a20710a
Samantha Acuña <samantha23@estudiantec.cr>
2025-04-22 14:20:27 -0600

    Actualizacion

---
+++ app/src/main/java/com/labs/applabs/student/Adapter/FormAdapter.kt
@@ -0,0 +1,25 @@
+package com.labs.applabs.student.Adapter
+
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.RecyclerView.ViewHolder
+import com.labs.applabs.student.Form
+
+class FormAdapter (private val forms : List<Form>) : RecyclerView.Adapter<FormAdapter.ViewHolder>() {
+
+
+    class ViewHolder(view: View) :RecyclerView.ViewHolder(view){
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int = forms.size
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+}
---
+++ app/src/main/java/com/labs/applabs/student/Form.kt
@@ -0,0 +1,14 @@
+package com.labs.applabs.student
+
+data class Form(val id: String,
+                val idStudent: String,
+                val idCard : Int,
+                val weightedAverage: Float,
+                val degree : String,
+                val idSchoolNumber : Int,
+                val shifts : Int,
+                val scheduleHours: List<String> = emptyList(),
+                val semestersOperator: Int = 0,
+                val psichologyName: String = "",
+                val ticketURL: String = ""
+)
