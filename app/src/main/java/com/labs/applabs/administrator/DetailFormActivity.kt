fb8023f2e750521ed9e1f49a02f9e35badf375e6
KatherineDAG <90380325+KatherineDAG@users.noreply.github.com>
2025-04-23 23:59:26 -0600

    Changes

--- app/src/main/java/com/labs/applabs/administrator/DetailFormActivity.kt
+++ app/src/main/java/com/labs/applabs/administrator/DetailFormActivity.kt
@@ -21,7 +21,7 @@ class DetailFormActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_detail_form)
-        showInfo("uM18B5gRWOygToYl7Uzy")
+        showInfo("UUli6leBpZyt5GFvHp7o")
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
