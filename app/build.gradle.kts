a03ca198167bff43a883def6a5f605014dca9f49
KatherineDAG <90380325+KatherineDAG@users.noreply.github.com>
2025-04-23 23:19:30 -0600

    strong changes

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -46,6 +46,7 @@ dependencies {
     implementation(libs.firebase.auth.ktx)
     implementation(libs.firebase.firestore)
     implementation(libs.firebase.auth)
+    implementation(libs.androidx.cardview)
 
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
