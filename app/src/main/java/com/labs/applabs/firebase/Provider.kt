cc643d72261d5b529ab898d5996ccf2a945f9d47
KatherineDAG <90380325+KatherineDAG@users.noreply.github.com>
2025-05-05 15:03:40 -0600

    Changes

--- app/src/main/java/com/labs/applabs/firebase/Provider.kt
+++ app/src/main/java/com/labs/applabs/firebase/Provider.kt
@@ -1,10 +1,59 @@
 package com.labs.applabs.firebase
+import android.content.Context
 import android.util.Log
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FirebaseFirestore
+import com.labs.applabs.student.FormStudentData
 import kotlinx.coroutines.tasks.await
 
 class Provider {
     private val db = FirebaseFirestore.getInstance()
+
+    // Funcion paraa poder obtener el id de la persona autenticada
+    private fun getAuthenticatedUserId(): String {
+        val currentUser = FirebaseAuth.getInstance().currentUser
+        return currentUser?.uid ?: throw IllegalStateException("No hay usuario autenticado")
+    }
+
+    fun saveStudentData(studentData: FormStudentData) {
+        val user = getAuthenticatedUserId()
+        try {
+            // Convertimos el objeto completo a un mapa compatible con Firestore
+            val dataMap = hashMapOf<String, Any>(
+                "idStudent" to user,
+                "idCard" to studentData.idCard,
+                "weightedAverage" to studentData.weightedAverage,
+                "degree" to studentData.degree,
+                "phoneNumber" to studentData.phoneNumber,
+                "IdSchoolNumber" to studentData.IdSchoolNumber,
+                "shift" to studentData.shift,
+                "semester" to studentData.semester,
+                "psychology" to studentData.psychology,
+                "ticketUrl" to studentData.ticketUrl,
+                "schedule" to studentData.schedule.map { daySchedule ->
+                    hashMapOf(
+                        "day" to daySchedule.day,
+                        "shifts" to daySchedule.shifts  // Lista de Strings directamente
+                    )
+                }
+            )
+
+
+            // Guardamos en Firestore
+            db.collection("Forms")
+                .add(dataMap)
+                .addOnSuccessListener {
+                    val context = this@Provider as? Context ?: return@addOnSuccessListener
+                    Toast.makeText(this@Provider, "¡Guardado!", Toast.LENGTH_SHORT).show() }
+                .addOnFailureListener {
+                    val context = this@Provider as? Context ?: return@addOnFailureListener
+                    Toast.makeText(this@Provider, "Error", Toast.LENGTH_LONG).show()}
+
+        } catch (e: Exception) {
+            Log.e("Firebase", "Error al guardar", e)
+        }
+    }
 
     suspend fun getUserInfo(userId: String?): DataClass? {
         return try {
@@ -81,6 +130,7 @@ class Provider {
 
 
     //suspend fun updateStatus(formId: String): Map<String, Any>? {}
+
 
 
 }
