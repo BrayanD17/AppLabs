48d1620732172fd56f8ac3f6db88e2cb0f3cfc3e
KatherineDAG <90380325+KatherineDAG@users.noreply.github.com>
2025-05-13 11:07:01 -0600

    Changes

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -51,6 +51,7 @@ dependencies {
     implementation(libs.firebase.storage.ktx)
     implementation(libs.firebase.functions)
     implementation(libs.firebase.messaging)
+    implementation(libs.firebase.messaging.ktx)
 
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
