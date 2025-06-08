c1c528e4a62f1a2616b6ee073860d0367ad2ab12
KatherineDAG <90380325+KatherineDAG@users.noreply.github.com>
2025-06-08 02:47:28 -0600

    Changes

---
+++ app/src/main/java/com/labs/applabs/administrator/menu_item_operator.kt
@@ -0,0 +1,30 @@
+package com.labs.applabs.administrator
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.ImageButton
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.labs.applabs.R
+import com.labs.applabs.operadores.HistorialOperadoresActivity
+
+class menu_item_operator : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_menu_item_operator)
+
+        //Call action btnICon
+        viewActiveOperator()
+    }
+
+    fun viewActiveOperator(){
+        val btnOperatorActive = findViewById<ImageButton>(R.id.btnOperatorActive)
+        btnOperatorActive.setOnClickListener {
+            val intent = Intent(this, HistorialOperadoresActivity::class.java)
+            startActivity(intent)
+        }
+    }
+}
