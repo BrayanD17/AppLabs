de1422da0abf292122d0e0907ff48e2c6ad185f8
Brayan Gutierrez Dinarte <38222878+BrayanD17@users.noreply.github.com>
2025-06-08 16:48:35 -0600

    Revert "Update"
    This reverts commit 31fc8030aacec968fa0710a7c0f6d711bcc58c6d.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -34,9 +34,6 @@ android {
     kotlinOptions {
         jvmTarget = "11"
     }
-    buildFeatures {
-        viewBinding = true
-    }
 }
 
 dependencies {
@@ -56,8 +53,6 @@ dependencies {
     implementation(libs.firebase.functions)
     implementation(libs.firebase.messaging)
     implementation(libs.firebase.messaging.ktx)
-    implementation(libs.androidx.navigation.fragment.ktx)
-    implementation(libs.androidx.navigation.ui.ktx)
 
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
