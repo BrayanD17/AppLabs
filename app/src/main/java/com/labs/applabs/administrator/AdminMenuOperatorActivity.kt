3a5492d5c0894feba6fb3ec5f48e5e8d9134b6cf
KatherineDAG <90380325+KatherineDAG@users.noreply.github.com>
2025-06-08 18:11:26 -0600

    Changes

---
+++ app/src/main/java/com/labs/applabs/administrator/AdminMenuOperatorActivity.kt
@@ -0,0 +1,25 @@
+package com.labs.applabs.administrator
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.labs.applabs.R
+
+class AdminMenuOperatorActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_admin_menu_operator)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+
+    fun actionBtnMenuBack(view: android.view.View) {
+        finish()
+    }
+}
