dd63e363373dcb63b14e7e7249a529b7b2a1b811
KatherineDAG <90380325+KatherineDAG@users.noreply.github.com>
2025-06-11 10:07:50 -0600

    Change

--- app/src/main/java/com/labs/applabs/operator/viewAssignedSchedule.kt
+++ app/src/main/java/com/labs/applabs/operator/viewAssignedSchedule.kt
@@ -11,13 +11,13 @@ class viewAssignedSchedule : AppCompatActivity() {
 
     private companion object {
         private val daysMap = mapOf(
-            "Lunes" to Triple(R.id.mondayMorning, R.id.mondayAfternoon, R.id.mondayEvening),
-            "Martes" to Triple(R.id.tuesdayMorning, R.id.tuesdayAfternoon, R.id.tuesdayEvening),
-            "Miércoles" to Triple(R.id.wednesdayMorning, R.id.wednesdayAfternoon, R.id.wednesdayEvening),
-            "Jueves" to Triple(R.id.thursdayMorning, R.id.thursdayAfternoon, R.id.thursdayEvening),
-            "Viernes" to Triple(R.id.fridayMorning, R.id.fridayAfternoon, R.id.fridayEvening),
-            "Sábado" to Triple(R.id.saturdayMorning, R.id.saturdayAfternoon, R.id.saturdayEvening),
-            "Domingo" to Triple(R.id.sundayMorning, R.id.sundayAfternoon, R.id.sundayEvening)
+            "Lunes" to Triple(R.id.DataMondayMorning, R.id.DataMondayAfternoon, R.id.DataMondayEvening),
+            "Martes" to Triple(R.id.DataTuesdayMorning, R.id.DataTuesdayAfternoon, R.id.DataTuesdayEvening),
+            "Miércoles" to Triple(R.id.DataWednesdayMorning, R.id.DataWednesdayAfternoon, R.id.DataWednesdayEvening),
+            "Jueves" to Triple(R.id.DataThursdayMorning, R.id.DataThursdayAfternoon, R.id.DataThursdayEvening),
+            "Viernes" to Triple(R.id.DataFridayMorning, R.id.DataFridayAfternoon, R.id.DataFridayEvening),
+            "Sábado" to Triple(R.id.DataSaturdayMorning, R.id.DataSaturdayAfternoon, R.id.DataSaturdayEvening),
+            "Domingo" to Triple(R.id.DataSundayMorning, R.id.DataSundayAfternoon, R.id.DataSundayEvening)
         )
     }
 
