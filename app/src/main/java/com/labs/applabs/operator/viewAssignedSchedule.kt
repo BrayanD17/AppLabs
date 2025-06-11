6287a4a84394bd895dd356f3a00f3f8325785651
KatherineDAG <90380325+KatherineDAG@users.noreply.github.com>
2025-06-11 10:02:18 -0600

    Changes

--- app/src/main/java/com/labs/applabs/operator/viewAssignedSchedule.kt
+++ app/src/main/java/com/labs/applabs/operator/viewAssignedSchedule.kt
@@ -8,6 +8,19 @@ import androidx.core.view.WindowInsetsCompat
 import com.labs.applabs.R
 
 class viewAssignedSchedule : AppCompatActivity() {
+
+    private companion object {
+        private val daysMap = mapOf(
+            "Lunes" to Triple(R.id.mondayMorning, R.id.mondayAfternoon, R.id.mondayEvening),
+            "Martes" to Triple(R.id.tuesdayMorning, R.id.tuesdayAfternoon, R.id.tuesdayEvening),
+            "Miércoles" to Triple(R.id.wednesdayMorning, R.id.wednesdayAfternoon, R.id.wednesdayEvening),
+            "Jueves" to Triple(R.id.thursdayMorning, R.id.thursdayAfternoon, R.id.thursdayEvening),
+            "Viernes" to Triple(R.id.fridayMorning, R.id.fridayAfternoon, R.id.fridayEvening),
+            "Sábado" to Triple(R.id.saturdayMorning, R.id.saturdayAfternoon, R.id.saturdayEvening),
+            "Domingo" to Triple(R.id.sundayMorning, R.id.sundayAfternoon, R.id.sundayEvening)
+        )
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
