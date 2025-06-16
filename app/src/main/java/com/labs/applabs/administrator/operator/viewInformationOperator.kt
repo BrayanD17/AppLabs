5bb0cda25174da739d49d0129548d53b1e48ab12
KatherineDAG <90380325+KatherineDAG@users.noreply.github.com>
2025-06-16 02:10:33 -0600

    Changes

--- app/src/main/java/com/labs/applabs/administrator/operator/viewInformationOperator.kt
+++ app/src/main/java/com/labs/applabs/administrator/operator/viewInformationOperator.kt
@@ -8,14 +8,16 @@ import android.widget.ArrayAdapter
 import android.widget.Button
 import android.widget.LinearLayout
 import android.widget.Spinner
+import android.widget.TextView
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.content.ContentProviderCompat.requireContext
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.lifecycle.lifecycleScope
 import com.labs.applabs.R
+import com.labs.applabs.elements.ToastType
+import com.labs.applabs.elements.toastMessage
 import com.labs.applabs.firebase.Provider
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
@@ -28,11 +30,36 @@ class viewInformationOperator : AppCompatActivity() {
     private lateinit var laboratorySpinner: Spinner
     private lateinit var btnConfirmLab: Button
     private lateinit var btnEditSchedule: Button
+    private lateinit var operatorActiveId: String
+    private var operatorName: String = ""
+    val colors = listOf("#FF6F61", "#6B5B95", "#88B04B", "#F7CAC9", "#92A8D1")
+
+    private companion object {
+        private val daysMap = mapOf(
+            "Lunes" to Triple(R.id.mondayMorningOperator, R.id.mondayAfternoonOperator,R.id.mondayEveningOperator),
+            "Martes" to Triple(R.id.tuesdayMorningOperator, R.id.tuesdayAfternoonOperator, R.id.tuesdayEveningOperator),
+            "Miércoles" to Triple(R.id.wednesdayMorningOperator, R.id.wednesdayAfternoonOperator, R.id.wednesdayEveningOperator),
+            "Jueves" to Triple(R.id.thursdayMorningOperator, R.id.thursdayAfternoonOperator, R.id.thursdayEveningOperator),
+            "Viernes" to Triple(R.id.fridayMorningOperator, R.id.fridayAfternoonOperator, R.id.fridayEveningOperator),
+            "Sábado" to Triple(R.id.saturdayMorningOperator, R.id.saturdayAfternoonOperator, R.id.saturdayEveningOperator),
+            "Domingo" to Triple(R.id.sundayMorningOperator, R.id.sundayAfternoonOperator, R.id.sundayEveningOperator)
+        )
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_view_information_operator)
+
+        val id = intent.getStringExtra("operatorId")
+        if (id == null) {
+            toastMessage("ID del operador no recibido", ToastType.ERROR)
+            finish()
+            return
+        }
+        operatorActiveId = id
+        getOperatorInfo(operatorActiveId)
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
@@ -48,38 +75,60 @@ class viewInformationOperator : AppCompatActivity() {
         // Configurar el botón de edición
         btnEditSchedule.setOnClickListener {
             lifecycleScope.launch {
-                loadLaboratories() // Carga los datos
-                toggleLabSelection() // Muestra el contenedor del Spinner
+                loadLaboratories()
+                toggleLabSelection()
             }
         }
 
+    }
+
+    fun getOperatorInfo(operatorId: String) {
+        val operatorGetName = findViewById<TextView>(R.id.textDataNameOperator)
+        val operatorIdCard = findViewById<TextView>(R.id.textDataIdOperator)
+        val operatorEmail = findViewById<TextView>(R.id.textDataEmailOperator)
+        val operatorPhone = findViewById<TextView>(R.id.textDataPhoneOperator)
+
+        lifecycleScope.launch {
+            val infoUser = provider.getUserInfo(operatorId)
+
+            infoUser?.let { info ->
+                val studentInfo = info.studentInfo
+                operatorName = "${studentInfo.studentName} ${studentInfo.surNames}"
+                operatorGetName.text= operatorName
+                operatorIdCard.text = studentInfo.studentCard
+                operatorEmail.text = studentInfo.studentEmail
+                operatorPhone.text = studentInfo.studentCard
+            }
+        }
     }
 
     private fun toggleLabSelection() {
         if (labSelectionContainer.visibility == View.VISIBLE) {
             labSelectionContainer.visibility = View.GONE
             btnEditSchedule.text = getString(R.string.editSchedule)
+            btnEditSchedule.backgroundTintList= getColorStateList(R.color.green)
         } else {
             labSelectionContainer.visibility = View.VISIBLE
             btnEditSchedule.text = getString(R.string.cancel)
+            btnEditSchedule.backgroundTintList= getColorStateList(R.color.red)
         }
     }
 
     private suspend fun loadLaboratories() {
         try {
-            // Paso 1: Obtener los datos del provider
-            val laboratories = provider.getLaboratoryName() // ← Aquí llamas a tu función
-
-            // Paso 2: Configurar el Spinner en el hilo principal
+            val laboratories = provider.getLaboratoryName()
             withContext(Dispatchers.Main) {
                 val adapter = ArrayAdapter(this@viewInformationOperator, android.R.layout.simple_spinner_item, laboratories).apply {setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item) }
 
                 laboratorySpinner.adapter = adapter
-
-                // Listener para mostrar el botón de confirmación
                 laboratorySpinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
                     override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
                         btnConfirmLab.visibility = View.VISIBLE
+                        btnConfirmLab.backgroundTintList= getColorStateList(R.color.green)
+                        btnConfirmLab.background=getDrawable(R.drawable.bg_button)
+                        btnConfirmLab.setOnClickListener {
+                            Toast.makeText(this@viewInformationOperator, "Laboratorio seleccionado: ${laboratories[position]}", Toast.LENGTH_SHORT).show()
+                        }
                     }
 
                     override fun onNothingSelected(parent: AdapterView<*>?) {
@@ -90,7 +139,7 @@ class viewInformationOperator : AppCompatActivity() {
 
         } catch (e: Exception) {
             withContext(Dispatchers.Main) {
-                //Toast.makeText(cont, "Error cargando laboratorios", Toast.LENGTH_SHORT).show()
+                Toast.makeText(this@viewInformationOperator, "Error al cargar los laboratorios: ${e.message}", Toast.LENGTH_SHORT).show()
                 Log.e("Laboratorios", "Error: ${e.message}")
             }
         }
